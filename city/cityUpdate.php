dc25d617b8489eafbdefdb5a61571909880f1156
natrixbasil <114161306+natrixbasil@users.noreply.github.com>
2023-03-06 21:21:13 +0300

    Create cityUpdate.php

---
+++ city/cityUpdate.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../class/city.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$item = new City($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$item->id = $data->id;
+
+// employee values
+$item->name = $data->name;
+
+if($item->updateCity()){
+    echo json_encode("User data updated.");
+} else{
+    echo json_encode("Data could not be updated");
+}
+?>
