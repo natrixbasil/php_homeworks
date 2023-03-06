e51af0a723a60eb389fd8186d3197ae1df861900
natrixbasil <114161306+natrixbasil@users.noreply.github.com>
2023-03-06 21:29:10 +0300

    Create userGet.php

---
+++ user/userGet.php
@@ -0,0 +1,37 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/database.php';
+include_once '../class/user.php';
+$database = new Database();
+$db = $database->getConnection();
+$items = new User($db);
+$stmt = $items->getUsers();
+$itemCount = $stmt->rowCount();
+
+echo json_encode($itemCount);
+if($itemCount > 0){
+
+    $userArr = array();
+    $userArr["body"] = array();
+    $userArr["itemCount"] = $itemCount;
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $e = array(
+            "id" => $id,
+            "username" => $username,
+            "name" => $name,
+            "city_id" => $city_id,
+        );
+        array_push($userArr["body"], $e);
+    }
+    echo json_encode($userArr);
+}
+else{
+    http_response_code(404);
+    echo json_encode(
+        array("message" => "No record found.")
+    );
+}
+?>
