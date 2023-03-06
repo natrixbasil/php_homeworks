b0614c1a09f2c7b9ae2984697a76976fc688ce20
natrixbasil <114161306+natrixbasil@users.noreply.github.com>
2023-03-06 21:28:44 +0300

    Create userUpdate.php

---
+++ user/userUpdate.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../class/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$item = new User($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$item->id = $data->id;
+
+// employee values
+$item->username = $data->username;
+$item->name = $data->name;
+$item->city_id = $data->city_id;
+
+if($item->updateUser()){
+    echo json_encode("User data updated.");
+} else{
+    echo json_encode("Data could not be updated");
+}
+?>
